865791a4e13c156a87167b6e1cb8592c42628376
JunHyung1206 <pjh120610@naver.com>
2025-10-23 21:35:43 +0900

    refactor(InputCarNames) : 이름 입력 리펙토링
    Reader에는 문자열만 입력, 그 이후의 검증 및 변환은 Converter에서 수행

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.converter.Converter;
 import racingcar.view.Printer;
 import racingcar.view.Reader;
 
@@ -11,13 +12,12 @@ import java.util.List;
 public class RacingCarGame {
 
     private int gameRounds;
-    private final List<RacingCar> racingCars;
+    private List<RacingCar> racingCars;
     private int leadPosition;
 
     RacingCarGame(){
         gameRounds = 0;
         leadPosition = 0;
-        racingCars = new ArrayList<>();
     }
 
     void process(){
@@ -27,14 +27,9 @@ public class RacingCarGame {
     }
 
     private void initialize() {
-        String[] carNames = Reader.inputCarNames();
-        String gameRounds = Reader.inputGameRounds();
-
-        // TODO: 입력값이 유효한지 검증을 하는 단계 필요
-
         // 입력값을 값에 맞게 객체로 변환하는 과정 수행
-        Arrays.stream(carNames).forEach(carName -> this.racingCars.add(new RacingCar(carName)));
-        this.gameRounds = Integer.parseInt(gameRounds);
+        this.racingCars = Converter.convertCarNames(Reader.inputCarNames());
+        this.gameRounds = Converter.convertGameRound(Reader.inputGameRounds());
     }
 
     private void playRacing() {
--- src/main/java/racingcar/view/Reader.java
+++ src/main/java/racingcar/view/Reader.java
@@ -3,10 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Reader {
-    public static String[] inputCarNames(){
+    public static String inputCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        return carNames.split(",");
+        return Console.readLine();
     }
 
     public static String inputGameRounds(){
